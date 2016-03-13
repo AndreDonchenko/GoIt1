ff7843250732a17ce11c8f56625f83db2eacd803
Andre Donchenko <andre.donchenko@gmail.com>
2016-03-13 17:32:14 +0200

    Java EE Lab 1 added

---
+++ src/ua/goit/andre/ee1/Measure/MeasureArrayList.java
@@ -0,0 +1,10 @@
+package ua.goit.andre.ee1.Measure;
+
+import java.util.ArrayList;
+
+public class MeasureArrayList extends MeasureList {
+    public MeasureArrayList() {
+        list = new ArrayList<>();
+    }
+}
+
---
+++ src/ua/goit/andre/ee1/Measure/MeasureHashSet.java
@@ -0,0 +1,9 @@
+package ua.goit.andre.ee1.Measure;
+
+import java.util.HashSet;
+
+public class MeasureHashSet extends MeasureCollection {
+    public MeasureHashSet () {
+        collection = new HashSet<>();
+    }
+}
